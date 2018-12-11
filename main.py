4b7e110df4598efb2f8eaf380cd97d0c6ed8e36a
Eko <swdev.bali@gmail.com>
2018-12-11 13:54:49 +0700

    Hari ke-2 Phase 1 Chart generation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pprint
 import time
 
 try:
+    #DATA ACQUISITON
     result = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode=SRTG&period=1W')
     if result.status_code == 200:
         data = json.loads(result.text)
@@ -15,15 +16,24 @@ try:
         print(data['ChartData'])
         chart_data = data['ChartData']
 
+        #DATA PROCESSING
         f = open('data.csv', 'w')
-        f.write('#Tanggal;Value\n')
+        # f.write('#Tanggal;Value\n')
         for d in chart_data:
             #print()
             tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
             value = d['Close']
             print(tanggal, ';', value)
-            f.write('"{}";{}\n'.format(tanggal, value))
+            f.write('"{}",{}\n'.format(tanggal, value))
         f.close()
-
 except Exception as ex:
     print(ex)
+
+#DATA VISUALIZATION
+#Matplotlib -> Pandas
+from pandas import Series
+from matplotlib import pyplot
+
+series = Series.from_csv('data.csv')
+pyplot.plot(series)
+pyplot.show()
