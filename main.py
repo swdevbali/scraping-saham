177dd90052d54aff1947614d4cc5148914aa8206
Soes Hari Putra <soeshari.putra@outlook.com>
2018-12-11 14:49:17 +0700

    Scraping Saham Collaboration

--- main.py
+++ main.py
@@ -2,27 +2,48 @@
 GET: data muncul jika url dijalankan lewat broswer
 POST: data tidak bs diambil lewat browesr, namun via <FORM/> POST
 """
-import requests
-import json
-import pprint
-import time
 
-try:
-    result = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode=SRTG&period=1W')
-    if result.status_code == 200:
-        data = json.loads(result.text)
-        pprint.pprint(data)
-        print(data['ChartData'])
-        chart_data = data['ChartData']
+def generate_chart(perusahaan, durasi='1W'):
+    """
 
-        f = open('data.csv', 'w')
-        f.write('#Tanggal;Value\n')
-        for d in chart_data:
-            tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
-            value = d['Close']
-            print(tanggal, ';', value)
-            f.write('"{}";{}\n'.format(tanggal, value))
-        f.close()
+    :return:
+    """
+    import requests
+    import json
+    import pprint
+    import time
 
-except Exception as ex:
-    print(ex)
+    url = 'https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode={}&period={}'\
+        .format(perusahaan, durasi)
+
+    try:
+        result = requests.get(url)
+        if result.status_code == 200:
+            data = json.loads(result.text)
+            pprint.pprint(data)
+            print(data['ChartData'])
+            chart_data = data['ChartData']
+
+            f = open('data.csv', 'w')
+            # f.write('#Tanggal;Value\n')
+            for d in chart_data:
+                tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
+                value = d['Close']
+                print(tanggal, ';', value)
+                f.write('"{}",{}\n'.format(tanggal, value))
+            f.close()
+
+    except Exception as ex:
+        print(ex)
+
+    #DATA VISUALIZATION
+    #Matplotlib -> Pandas
+
+    from pandas import Series
+    from matplotlib import pyplot
+
+    series = Series.from_csv('data.csv')
+    pyplot.plot(series)
+    pyplot.show()
+
+generate_chart('BBNI', durasi='1Y')
