9a5372ed4dd55869b3799c2d03844dd8c7ee680f
Eko <swdev.bali@gmail.com>
2018-12-11 14:27:12 +0700

    Hari ke-2 Phase 1 Dynamic chart

--- main.py
+++ main.py
@@ -2,38 +2,49 @@
 GET: data muncul jika url dijalankan lewat broswer
 POST: data tidak bs diambil lewat browesr, namun via <FORM/> POST
 """
-import requests
-import json
-import pprint
-import time
 
-try:
-    #DATA ACQUISITON
-    result = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode=SRTG&period=1W')
-    if result.status_code == 200:
-        data = json.loads(result.text)
-        pprint.pprint(data)
-        print(data['ChartData'])
-        chart_data = data['ChartData']
+def generate_chart(perusahaan, durasi='1W'):
+    """
+    Fungsi ini akan menghasilkan image dari data stock seminggu terakhir dari perusahaan tertentu
+    :return:
+    """
+    import requests
+    import json
+    import pprint
+    import time
 
-        #DATA PROCESSING
-        f = open('data.csv', 'w')
-        f.write('#Tanggal;Value\n')
-        for d in chart_data:
-            #print()
-            tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
-            value = d['Close']
-            print(tanggal, ';', value)
-            f.write('"{}",{}\n'.format(tanggal, value))
-        f.close()
-except Exception as ex:
-    print(ex)
+    url = 'https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode={}&period={}'\
+        .format(perusahaan, durasi)
 
-#DATA VISUALIZATION
-#Matplotlib -> Pandas
-from pandas import Series
-from matplotlib import pyplot
+    try:
+        #DATA ACQUISITON
+        result = requests.get(url)
+        if result.status_code == 200:
+            data = json.loads(result.text)
+            pprint.pprint(data)
+            print(data['ChartData'])
+            chart_data = data['ChartData']
 
-series = Series.from_csv('data.csv', header=0, sep=',')
-pyplot.plot(series)
-pyplot.show()
+            #DATA PROCESSING
+            f = open('data.csv', 'w')
+            f.write('#Tanggal;Value\n')
+            for d in chart_data:
+                #print()
+                tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
+                value = d['Close']
+                print(tanggal, ';', value)
+                f.write('"{}",{}\n'.format(tanggal, value))
+            f.close()
+    except Exception as ex:
+        print(ex)
+
+    #DATA VISUALIZATION
+    #Matplotlib -> Pandas
+    from pandas import Series
+    from matplotlib import pyplot
+
+    series = Series.from_csv('data.csv', header=0, sep=',')
+    pyplot.plot(series)
+    pyplot.show()
+
+generate_chart('BBCA', durasi='1Y')
