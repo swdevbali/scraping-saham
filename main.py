883b6a2305595cbf4ac232d606a718eb2a5faa65
RWita <wita_76@yahoo.com>
2018-12-11 13:05:00 +0700

    day2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ POST: data tidak bs diambil lewat browesr, namun via <FORM/> POST
 import requests
 import json
 import pprint
-
+import time
 try:
     result = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode=SRTG&period=1W')
     if result.status_code == 200:
@@ -17,10 +17,11 @@ try:
         f = open('data.csv', 'w')
         f.write('#Tanggal;Value\n')
         for d in chart_data:
-            tanggal = d['Date']
+            tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date']) / 1000))
             value = d['Close']
             print(tanggal, ';', value)
-            f.write('{};{}\n'.format(tanggal, value))
+            f.write('"{}";{}\n'.format(tanggal, value))
+            #f.write('{};{}\n'.format(tanggal, value))
         f.close()
 
 except Exception as ex:
