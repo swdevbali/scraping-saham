2c01c4eac8ef61a14ccce5a88a1f0ba5857d4bee
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2018-12-11 10:59:24 +0700

    Hari ke-2 Phase 1 Scraping

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+GET: data muncul jika url dijalankan lewat broswer
+POST: data tidak bs diambil lewat browesr, namun via <FORM/> POST
+"""
+import requests
+import json
+import pprint
+
+try:
+    result = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode=SRTG&period=1W')
+    if result.status_code == 200:
+        data = json.loads(result.text)
+        pprint.pprint(data)
+        print(data['ChartData'])
+        chart_data = data['ChartData']
+
+        f = open('data.csv', 'w')
+        f.write('#Tanggal;Value\n')
+        for d in chart_data:
+            tanggal = d['Date']
+            value = d['Close']
+            print(tanggal, ';', value)
+            f.write('{};{}\n'.format(tanggal, value))
+        f.close()
+
+except Exception as ex:
+    print(ex)
