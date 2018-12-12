b16423627267a368b9ae43f54e50dce6ccea3722
visynu <visynu.budhi@bpk.go.id>
2018-12-12 10:34:29 +0700

    Hari 3 tahap 1

---
+++ serversaham/sahamapp/views.py
@@ -0,0 +1,62 @@
+import io
+import matplotlib
+import os
+
+from django.http import HttpResponse
+from matplotlib import pyplot
+from pandas import Series
+
+
+def get_chart(requests, perusahaan, durasi):
+    """
+    Fungsi ini akan menghasilkan image dari data stock seminggu terakhir dari perusahaan tertentu
+    :return:
+    """
+    import requests
+    import json
+    import pprint
+    import time
+
+    url = 'https://www.idx.co.id/umbraco/Surface/Helper/GetStockChart?indexCode={}&period={}'\
+        .format(perusahaan, durasi)
+
+    try:
+        #DATA ACQUISITON
+        result = requests.get(url)
+        if result.status_code == 200:
+
+            data = json.loads(result.text)
+
+            pprint.pprint(data)
+            print(data['ChartData'])
+            chart_data = data['ChartData']
+
+            #DATA PROCESSING
+            file_path = os.path.join(os.getcwd(), 'data.csv')
+            f = open(file_path, 'w')
+            f.write('#Tanggal;Value\n')
+
+            for d in chart_data:
+                #print()
+                tanggal = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(d['Date'])/1000))
+                value = d['Close']
+                print(tanggal, ';', value)
+                f.write('"{}",{}\n'.format(tanggal, value))
+            f.close()
+    except Exception as ex:
+        print(ex)
+
+    #DATA VISUALIZATION
+    f = matplotlib.figure.Figure()
+    series = Series.from_csv('data.csv', header=0, sep=',')
+    pyplot.clf()
+    pyplot.plot(series)
+
+    #Django fix: create in buffer first
+    buf = io.BytesIO()
+    pyplot.savefig(buf, format='png')
+    pyplot.close(f)
+
+    response = HttpResponse(buf.getvalue(), content_type='image/png')
+    return response
+
